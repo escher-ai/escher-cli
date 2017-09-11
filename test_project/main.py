abc9006a468cd59861120719ca200551f0ce4529
Ge Yang <yangge1987@gmail.com>
2017-09-11 03:25:52 -0500

    fix timestamp and RunTimeParams evaluation

--- test_project/main.py
+++ test_project/main.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import sys
+from time import sleep
 
 from moleskin import Moleskin
 
@@ -13,9 +14,11 @@ parser = argparse.ArgumentParser(description='Process some integers.')
 parser.add_argument('--num-tasks', type=int)
 parser.add_argument('--num-grad-steps', type=int)
 parser.add_argument('--num-points-sampled', type=int)
+parser.add_argument('--run-id', type=str)
 
 args, unknown_args = parser.parse_known_args()
 M.yellow(args, unknown_args)
+M.green(args.run_id)
 try:
     assert args.num_tasks == 20
     assert args.num_grad_steps == 3
@@ -24,3 +27,5 @@ except AssertionError as e:
     assert args.num_tasks == 10
     assert args.num_grad_steps == 1
     assert args.num_points_sampled == 10
+
+sleep(0.5)
