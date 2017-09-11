8fd20c2d2826fb913ebe55d250dc2f8911cb169c
Ge Yang <yangge1987@gmail.com>
2017-09-11 02:50:04 -0500

    Fix test

--- bin/test_dave.py
+++ bin/test_dave.py
@@ -1,19 +1,22 @@
+import sys
 from subprocess import check_call, CalledProcessError
 
 
 def test_dave_36():
-    try:
-        check_call("cd ../test_project && source activate simulation && python "
-                   "../bin/dave --config-file=experiment.yml", shell=True)
-    except CalledProcessError as e:
+    if sys.version_info >= (3, 6):
         print('python 3.6')
-        print(e)
+        try:
+            check_call("cd ../test_project && source activate simulation && python "
+                       "../bin/dave --config-file=experiment.yml", shell=True)
+        except CalledProcessError as e:
+            print(e)
 
 
 def test_dave_35():
-    try:
-        check_call("cd ../test_project && source activate gym && python "
-                   "../bin/dave --config-file=experiment.yml", shell=True)
-    except CalledProcessError as e:
+    if sys.version_info < (3, 6):
         print('python 3.5')
-        print(e)
+        try:
+            check_call("cd ../test_project && source activate gym && python "
+                       "../bin/dave --config-file=experiment.yml", shell=True)
+        except CalledProcessError as e:
+            print(e)
--- test_project/main.py
+++ test_project/main.py
@@ -2,6 +2,10 @@ import argparse
 import os
 import sys
 
+from moleskin import Moleskin
+
+M = Moleskin()
+
 test_directory = os.getcwd()
 assert test_directory in sys.path, "test_directory should be inside the system path."
 
@@ -10,7 +14,13 @@ parser.add_argument('--num-tasks', type=int)
 parser.add_argument('--num-grad-steps', type=int)
 parser.add_argument('--num-points-sampled', type=int)
 
-args, unknow_args = parser.parse_known_args()
-assert args.num_tasks == 20
-assert args.num_grad_steps == 3
-assert args.num_points_sampled == 50
+args, unknown_args = parser.parse_known_args()
+M.yellow(args, unknown_args)
+try:
+    assert args.num_tasks == 20
+    assert args.num_grad_steps == 3
+    assert args.num_points_sampled == 50
+except AssertionError as e:
+    assert args.num_tasks == 10
+    assert args.num_grad_steps == 1
+    assert args.num_points_sampled == 10
